b29576b6234efd1387e4a44e225bfa9d814bfba6
ariel <ariel@Ariel-Maldonado>
2022-05-17 19:53:13 -0500

    Archivos necesarios para el proyecto

---
+++ src/taller1_repo_am/Principal.java
@@ -0,0 +1,10 @@
+package taller1_repo_am;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto JAVA en GitHub");
+	}
+
+}
